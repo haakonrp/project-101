04e43f330bb09b7b1bf4f2754ee8900ba6e0a6a0
Håkon Rosseland Paulsen <hakon.r.paulsen@accenture.com>
2020-09-03 13:36:50 +0200

    Add auto populate company with employees when creating company by orgnr

---
+++ backend/integration/src/main/java/no/acntech/project101/company/model/MemberList.java
@@ -0,0 +1,14 @@
+package no.acntech.project101.company.model;
+
+public class MemberData {
+
+    private String person;
+
+    public String getPerson() {
+        return person;
+    }
+
+    public void setPerson(final String person) {
+        this.person = person;
+    }
+}
